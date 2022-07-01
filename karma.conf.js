6490372fbdfaa64c0e66fc4b001c3d8786cb1573
Aram <aram@novanoweb.com>
2022-07-01 21:53:04 +0400

    changed project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sniper'),
+      dir: require('path').join(__dirname, './coverage/henKiller'),
       subdir: '.',
       reporters: [
         { type: 'html' },
